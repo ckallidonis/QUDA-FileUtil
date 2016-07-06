96fdd7021800b25fad230528f79a45fd04ca3107
ckallidonis <kallidonis@gmail.com>
2016-07-06 16:37:12 +0200

    Merged extract_exact_loop_h5.sh and reshape_exact_loop_h5.sh into extract_exact_loop_h5.sh

--- scripts_h5/loop/extract_exact_loop_h5.sh
+++ scripts_h5/loop/extract_exact_loop_h5.sh
@@ -2,28 +2,34 @@
 
 if [ $# -ne 6 ]
 then
-    echo "Usage: $0 <h5-file> <extract_dir> <mom_list> <exe_dir> <T> <traj>"
+    echo "Usage: $0 <h5-file> <extract_dir> <out_dir> <mom_list> <exe_dir> <T> <traj>"
     exit
 fi
 
 
 FILE=$1
-EDIR=$2
-MOM_LIST=$3
-EXE_DIR=$4
-T=$5
-TRAJ=$6
+DIR=$2
+OUT_DIR=$3
+MOM_LIST=$4
+EXE_DIR=$5
+T=$6
+TRAJ=$7
 
-dir=${EDIR}/exact_hdf5_extracted
+EX_DIR=${DIR}/exact_hdf5_extracted
 
-mkdir -p ${dir}
+mkdir -p ${EX_DIR}
 
 for tp in Scalar dOp Loops LoopsCv LpsDw LpsDwCv
 do
+    OUT_FILE=${OUT_DIR}/loop_exact.${TRAJ}_${tp}.loop.dat
+    rm -f ${OUT_FILE}
+
     while read mx my mz
     do
-	OUT_FILE=${dir}/loop_exact.${TRAJ}_${tp}.${mx}_${my}_${mz}.loop.dat
+	EX_FILE=${EX_DIR}/loop_exact.${TRAJ}_${tp}.${mx}_${my}_${mz}.loop.dat
 	    
-	${EXE_DIR}/extract_exact_loop_h5 ${FILE} ${OUT_FILE} ${tp} ${mx} ${my} ${mz} ${TRAJ} ${T}
+	${EXE_DIR}/extract_exact_loop_h5 ${FILE} ${EX_FILE} ${tp} ${mx} ${my} ${mz} ${TRAJ} ${T}
+
+	cat ${EX_FILE} >> ${OUT_FILE}
     done < ${MOM_LIST}
 done
