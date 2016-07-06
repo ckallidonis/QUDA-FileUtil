8fdb93b617fe04b2e4df8a9645101685fbfb5407
ckallidonis <kallidonis@gmail.com>
2016-07-06 15:58:28 +0200

    Merged extract_threep_h5.sh and reshape_threep_h5.sh into extract_threep_h5.sh

--- scripts_h5/threep/extract_threep_h5.sh
+++ scripts_h5/threep/extract_threep_h5.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-if [ $# -ne 8 ]
+if [ $# -ne 9 ]
 then
-    echo "Usage: $0 <extact_dir> <src_list> <mom_list> <conf> <tsink> <proj> <Qsq> <exe_dir>"
+    echo "Usage: $0 <extact_dir> <out_dir> <src_list> <mom_list> <conf> <tsink> <proj> <Qsq> <exe_dir>"
     exit
 fi
 
 DIR=$1
-SRC_LIST=$2
-MOM_LIST=$3
-CONF=$4
-TSINK=$5
-PROJ=$6
-Qsq=$7
-EXE_DIR=$8
+OUT_DIR=$2
+SRC_LIST=$3
+MOM_LIST=$4
+CONF=$5
+TSINK=$6
+PROJ=$7
+Qsq=$8
+EXE_DIR=$9
 
 while read x y z t
 do 
@@ -21,9 +22,9 @@ do
 
     src=sx${x}sy${y}sz${z}st${t}
 
-    OUT_DIR=${DIR}/${src}
+    EX_DIR=${DIR}/${src}
 
-    mkdir -p ${OUT_DIR}
+    mkdir -p ${EX_DIR}
 
     for tsink in $TSINK
     do 
@@ -33,11 +34,16 @@ do
 	    do
 		for tp in ultra_local noether oneD
 		do
+		    OUT_FILE=${OUT_DIR}/threep.${CONF}_tsink${tsink}_proj${proj}.neutron.${part}.${tp}.SS.${x}.${y}.${z}.${t}.dat
+		    rm -f ${OUT_FILE}
+
 		    while read mx my mz
 		    do
-			OUT_FILE=${OUT_DIR}/threep.${CONF}_tsink${tsink}_proj${proj}.neutron.${part}.${tp}.${mx}_${my}_${mz}.SS.${x}.${y}.${z}.${t}.dat
+			EX_FILE=${EX_DIR}/threep.${CONF}_tsink${tsink}_proj${proj}.neutron.${part}.${tp}.${mx}_${my}_${mz}.SS.${x}.${y}.${z}.${t}.dat
 			
-			${EXE_DIR}/extract_threep_h5 ${FILE} ${OUT_FILE} ${tp} ${proj} ${part} ${mx} ${my} ${mz} ${CONF} ${src} ${tsink}
+			${EXE_DIR}/extract_threep_h5 ${FILE} ${EX_FILE} ${tp} ${proj} ${part} ${mx} ${my} ${mz} ${CONF} ${src} ${tsink}
+
+			cat ${EX_FILE} >> ${OUT_FILE}
 		    done < ${MOM_LIST}
 		done
 	    done
