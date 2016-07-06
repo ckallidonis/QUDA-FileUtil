05f03a6235fbcea37276deeabee22c219111a2cb
ckallidonis <kallidonis@gmail.com>
2016-07-06 16:37:47 +0200

    Merged extract_stoch_loop_h5.sh and reshape_stoch_loop_h5.sh into extract_stoch_loop_h5.sh

--- scripts_h5/loop/extract_stoch_loop_h5.sh
+++ scripts_h5/loop/extract_stoch_loop_h5.sh
@@ -2,32 +2,38 @@
 
 if [ $# -ne 7 ]
 then
-    echo "Usage: $0 <h5-file> <extract_dir> <mom_list> <stoch_list> <exe_dir> <T> <traj>"
+    echo "Usage: $0 <h5-file> <extract_dir> <out_dir> <mom_list> <stoch_list> <exe_dir> <T> <traj>"
     exit
 fi
 
 
 FILE=$1
-EDIR=$2
-MOM_LIST=$3
-NSTOCH_LIST=$4
-EXE_DIR=$5
-T=$6
-TRAJ=$7
+DIR=$2
+OUT_DIR=$3
+MOM_LIST=$4
+NSTOCH_LIST=$5
+EXE_DIR=$6
+T=$7
+TRAJ=$8
 
-dir=${EDIR}/stoch_hdf5_extracted
+EX_DIR=${DIR}/stoch_hdf5_extracted
 
-mkdir -p ${dir}
+mkdir -p ${EX_DIR}
 
 for tp in Scalar dOp Loops LoopsCv LpsDw LpsDwCv
 do
     for Ns in `cat $NSTOCH_LIST`
     do
+	OUT_FILE=${OUT_DIR}/loop_stoch.${TRAJ}_${tp}.loop.${Ns}.dat
+	rm -f ${OUT_FILE}
+
 	while read mx my mz
 	do
-	    OUT_FILE=${dir}/loop_stoch.${TRAJ}_${tp}.${mx}_${my}_${mz}.loop.${Ns}.dat
+	    EX_FILE=${EX_DIR}/loop_stoch.${TRAJ}_${tp}.${mx}_${my}_${mz}.loop.${Ns}.dat
 	    
-	    ${EXE_DIR}/extract_stoch_loop_h5 ${FILE} ${OUT_FILE} ${tp} ${mx} ${my} ${mz} ${TRAJ} ${Ns} ${T}
+	    ${EXE_DIR}/extract_stoch_loop_h5 ${FILE} ${EX_FILE} ${tp} ${mx} ${my} ${mz} ${TRAJ} ${Ns} ${T}
+
+	    cat ${EX_FILE} >> ${OUT_FILE}
 	done < ${MOM_LIST}
     done
 done
